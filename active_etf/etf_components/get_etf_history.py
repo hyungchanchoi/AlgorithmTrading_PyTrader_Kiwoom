0e577428a98017b1c697815d5f51d38765b12c76
hyungchanchoi <chhch13@gmail.com>
2021-01-11 00:18:07 +0900

    Add etf components(210111)

--- active_etf/etf_components/get_etf_history.py
+++ active_etf/etf_components/get_etf_history.py
@@ -1,11 +1,3 @@
-
-# coding: utf-8
-
-# # 데이터 저장
-
-# In[1]:
-
-
 import pandas as pd
 from urllib.request import urlopen
 import bs4
@@ -16,7 +8,7 @@ etf = ['kospi','kodex','tiger']
 url = ['https://wisefn.finance.daum.net/v1/ETF/index.aspx?cmp_cd=069500',
        'https://wisefn.finance.daum.net/v1/ETF/index.aspx?cmp_cd=364690',
        'https://wisefn.finance.daum.net/v1/ETF/index.aspx?cmp_cd=365040']
-with pd.ExcelWriter('C:/Users/chhch/algorithmtrading/active_etf/etf_history/'+str(today)+'.xlsx') as writer:
+with pd.ExcelWriter('C:/Users/chhch/algorithmtrading/PyTrader_KIWOOM/active_etf/etf_components/'+str(today)+'.xlsx') as writer:
     
     for j in range(len(etf)):
         source = urlopen(url[j]).read()
@@ -36,32 +28,3 @@ with pd.ExcelWriter('C:/Users/chhch/algorithmtrading/active_etf/etf_history/'+st
         temp['num']=num
         temp['per']=per
         temp.to_excel(writer, sheet_name=etf[j])
-
-
-# # 데이터 분석
-
-# In[16]:
-
-
-# kospi = pd.read_excel('C:/Users/chhch/algorithmtrading/active_etf/etf_history/'+str(today)+'.xlsx', sheet_name = 'kospi')
-# kodex = pd.read_excel('C:/Users/chhch/algorithmtrading/active_etf/etf_history/'+str(today)+'.xlsx', sheet_name = 'kodex')
-# tiger = pd.read_excel('C:/Users/chhch/algorithmtrading/active_etf/etf_history/'+str(today)+'.xlsx', sheet_name = 'tiger')
-
-
-# In[30]:
-
-
-# notin_kodex = []; notin_tiger=[]
-# for i in list(kospi['name']):
-#     if i not in list(kodex['name']):
-#         notin_kodex.append(i)
-#     if i not in list(tiger['name']):
-#         notin_tiger.append(i)
-    
-
-
-# In[31]:
-
-
-notin_tiger
-
